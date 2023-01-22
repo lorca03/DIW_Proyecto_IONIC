8f4c30ac44b805a0d039b2906cd5793102932789
lorca03 <guillermolorcamartinez@gmail.com>
2023-01-22 12:27:21 +0100

    home header

--- MONEDA/proyecto/src/app/app-routing.module.ts
+++ MONEDA/proyecto/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'registration',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
@@ -18,10 +18,6 @@ const routes: Routes = [
   {
     path: 'registration',
     loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
-  },
-  {
-    path: 'forgot-password',
-    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   },
 ];
 

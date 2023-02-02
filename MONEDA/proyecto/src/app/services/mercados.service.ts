3a0a1a7776764a2a2c6934ea946d9c59e2bf057a
lorca03 <guillermolorcamartinez@gmail.com>
2023-02-02 11:28:24 +0100

    rates

--- MONEDA/proyecto/src/app/services/mercados.service.ts
+++ MONEDA/proyecto/src/app/services/mercados.service.ts
@@ -1,25 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MercadosService {
 
-  public host :string='api.frankfurter.app';
-  public apiKey :string='7c83dc79e551e3f4074131f1b14fe9a3';
   constructor(private http:HttpClient) { }
 
   mercados(){
-    this.http.get(`https://${this.host}/latest?to=USD`)
-    .subscribe(
-      (resp:any)=>{
-      console.log(resp.rates);
-    });
-    // fetch(`https://api.frankfurter.app/latest?amount=10&from=GBP&to=USD`) 
-    // .then(resp => resp.json()) 
-    // .then((data) => { 
-    //   alert(`10 GBP = ${data.rates.USD} USD`); 
-    // });
+    const peticion=this.http.get<String>(`https://api.frankfurter.app/currencies`)
+    return peticion
+  }
+  divisia(query:string){
+    return this.http.get(`https://api.frankfurter.app/latest?to=${query}`);
   }
 }
--- MONEDA/proyecto/src/app/stats/stats.page.ts
+++ MONEDA/proyecto/src/app/stats/stats.page.ts
@@ -7,15 +7,39 @@ import { MercadosService } from '../services/mercados.service';
   styleUrls: ['./stats.page.scss'],
 })
 export class StatsPage implements OnInit {
-
-  public atras:string='home'
-  constructor(private mercadosService:MercadosService) {
-  }
+  public atras: string = 'home';
+  public monedasmostrar: string[] = ['USD', 'JPY', 'GBP', 'CHF', 'AUD'];
+  public fotos: string[] = ['america.png', 'china.PNG', 'GBP', 'CHF', 'AUD'];
+  public precios: string[] = [];
+  public nombresmonedas:string[]=[]
+  constructor(private mercadosService: MercadosService) {}
 
   ngOnInit(): void {
-   this.mercadosService.mercados()
-    console.log('fdg');
+    this.idmonedas()
+    this.monedasmostrar.forEach(element => {
+      this.latest(element)
+    });
+    console.log(this.precios);
     
   }
-
+  latest(element:any){
+     this.mercadosService.divisia(element).subscribe(
+      (resp:any)=>{
+        this.precios.push(resp.rates);
+    })
+  }
+  idmonedas(){
+    this.mercadosService.mercados().subscribe(res=>{
+      for (let index = 0; index < this.monedasmostrar.length; index++) {
+        const element2 = this.monedasmostrar[index];
+        for (let index = 0; index < Object.entries(res).length; index++) {
+          if (element2==Object.entries(res)[index][0]) {
+            this.nombresmonedas.push(Object.entries(res)[index][1])
+          }
+        }
+      }
+    });
+  }
 }
+
+
